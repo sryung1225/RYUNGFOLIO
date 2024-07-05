54f9ddc9172326f2e3d04ba2531f40c00133235a
sryung <sryung1225@gmail.com>
2024-07-05 19:33:54 +0900

    [🔧 setting] 외부이미지 next/image 사용을 위한 도메인 지정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["placehold.co"],
+  },
   sassOptions: {
     includePaths: ["styles"],
     prependData: `@import "@/styles/variables.scss"; @import "@/styles/mixins.scss";`,
