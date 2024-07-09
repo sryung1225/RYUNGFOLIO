1232c23d273237799572d2b0d7df9a476c5424f2
sryung <sryung1225@gmail.com>
2024-07-10 01:51:25 +0900

    [🎨 style] 타이틀 문장 구조 변경 #3

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,12 +4,14 @@ export default function Intro() {
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.title}>
-        안녕하세요
-        <br />
-        항상 더 나은 길을 고민하는
-        <br />웹 <span className={styles.point_red}>프론트엔드</span> 개발자
-        <br />
-        <span className={styles.point_green}>이성령</span> 입니다
+        <p>안녕하세요</p>
+        <p>항상 더 나은 길을 고민하는</p>
+        <p>
+          웹 <span className={styles.point_red}>프론트엔드</span> 개발자
+        </p>
+        <p>
+          <span className={styles.point_green}>이성령</span> 입니다
+        </p>
       </h2>
     </section>
   );
