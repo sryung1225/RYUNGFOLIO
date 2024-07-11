e212f51c7401a66fd0e48080db9369f4d645ad4f
sryung <sryung1225@gmail.com>
2024-07-12 03:26:19 +0900

    [🎨 style] 헤더 마크업 및 기본 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/common/Header';
 import Intro from '@/components/intro/Intro';
 import About from '@/components/About/About';
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <Intro />
       <About />
     </main>
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import styles from '@/styles/components/common/Header.module.scss';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Header() {
+  const [openNav, setOpenNav] = useState(false);
+  const toggleNav = () => {
+    if (openNav) {
+      setOpenNav(false);
+    } else {
+      setOpenNav(true);
+    }
+  };
+  return (
+    <header className={styles.header}>
+      <Image
+        className={styles.logo}
+        src="/img/logo.svg"
+        alt="로고"
+        width="40"
+        height="40"
+      />
+      <button type="button" className={styles.navButton} onClick={toggleNav}>
+        <Image src="/img/nav.svg" alt="네비게이션" width="30" height="26" />
+      </button>
+      {openNav && <nav className={styles.nav}></nav>}
+    </header>
+  );
+}
