a39868116e313e1848fa598012664653f6eb7c78
sryung <sryung1225@gmail.com>
2024-07-16 05:25:53 +0900

    [⚙️ chore] 헤드라인 컴포넌트 분리 #6

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,28 +1,23 @@
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import styles from '@/styles/components/common/Section.module.scss';
+import Headline from './Headline';
 
 interface SectionProps {
   id: string;
-  headlineText: ReactNode;
-  subheadlineText: string;
+  headline: ReactNode;
+  subheadline: ReactNode | null;
   children: ReactNode;
 }
 
-function Section({
-  id,
-  headlineText,
-  subheadlineText,
-  children,
-}: SectionProps) {
+function Section({ id, headline, subheadline, children }: SectionProps) {
   return (
     <section id={id} className={styles.section}>
       <h2 className="a11yHidden">프로필</h2>
       <Link href={`#${id}`} className={styles.hashtag}>
         # {id.toUpperCase()}
       </Link>
-      <h3 className={styles.headline}>{headlineText}</h3>
-      <h4 className={styles.subheadline}>{subheadlineText}</h4>
+      <Headline headline={headline} subheadline={subheadline} />
       {children}
     </section>
   );
