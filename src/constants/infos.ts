f1b3ccc4d5914ac3a387a90ae4beda814d50b6e3
sryung <sryung1225@gmail.com>
2024-07-16 05:28:40 +0900

    [🎨 style] About 섹션 마크업 및 스타일링 #6

---
+++ src/constants/infos.ts
@@ -0,0 +1,31 @@
+const infos = [
+  {
+    key: 'birth',
+    label: '생일',
+    description: '1996년 12월 25일생. 크리스마스가 생일이에요',
+  },
+  {
+    key: 'location',
+    label: '거주지',
+    description: '25년째 경기도 용인시에서 거주 중입니다',
+  },
+  {
+    key: 'mbti',
+    label: 'MBTI',
+    description:
+      'MBTI는 ISFJ로 사람들과의 화합을 중요시 해요. 결국에는 친절이 세상을 구한다!',
+  },
+  {
+    key: 'swim',
+    label: '수영',
+    description: '수영 1년차. 아침 수영 반에서 하루를 시작해요',
+  },
+  {
+    key: 'volunteer',
+    label: '봉사활동',
+    description:
+      '현재 경기도 청년 봉사단 소속으로 월 1회 봉사활동을 목표로 해요',
+  },
+];
+
+export default infos;
