7aca9257c81de429e240b66eca09eba1ed493abc
sryung <sryung1225@gmail.com>
2024-07-18 20:04:57 +0900

    [⏪ revert] 디자인 리뉴얼로 인한 about 섹션 작업 원복 #7

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,9 +1,7 @@
 import styles from '@/styles/components/about/About.module.scss';
 import Image from 'next/image';
-import infos from '@/constants/infos';
 import React from 'react';
 import Section from '../common/Section';
-import Headline from '../common/Headline';
 import Profile from './Profile';
 
 export default function About() {
@@ -25,50 +23,8 @@ export default function About() {
           width="260"
           height="260"
         />
-        <Image
-          src="/img/profile_2.jpg"
-          alt="프로필 이미지"
-          width="260"
-          height="260"
-        />
       </div>
       <Profile />
-      <dl className={styles.info_wrapper}>
-        {infos.map((info) => (
-          <React.Fragment key={info.key}>
-            <dt className={styles.info_icon}>
-              <i className={styles[info.key]} />
-              <span className="a11yHidden">{info.label}</span>
-            </dt>
-            <dd className={styles.info_description}>{info.description}</dd>
-          </React.Fragment>
-        ))}
-      </dl>
-      <Headline
-        headline={
-          <>
-            그리고 <span className="point_green">개발자</span> 이성령을
-            소개합니다.
-          </>
-        }
-        subheadline={
-          <>
-            더 좋은 방식이 있지 않을까에 대한 끊임 없는 고민을 합니다.
-            <br />
-            그리고 내가 안 바를 모두에게 쉬운 말로 공유하는 것을 좋아합니다.
-          </>
-        }
-      />
-      <div className={styles.gallery}>
-        <Image
-          src="/img/profile_3.png"
-          alt="프로필 미모티콘"
-          width="300"
-          height="310"
-        />
-        <div className={styles.bubble_left}>깃헙</div>
-        <div className={styles.bubble_right}>블로그</div>
-      </div>
     </Section>
   );
 }
