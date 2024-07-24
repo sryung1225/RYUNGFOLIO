fced2d0c28c69b9883f6805ef8599536fa3253a5
sryung <sryung1225@gmail.com>
2024-07-24 19:19:59 +0900

    [⏪ revert] 리스트 토글 관련 코드 원복 #10
    - 분량 불균형 및 컴포넌트 관리를 위하여 스펙 아웃 결정

--- src/components/work/ExperienceCard.tsx
+++ src/components/work/ExperienceCard.tsx
@@ -1,42 +1,14 @@
-'use client';
-
 import Image from 'next/image';
-import { useState } from 'react';
 import WORK_EXPEREINCES from '@/constants/work_experience';
 import styles from '@/styles/components/work/ExperienceCard.module.scss';
 import { CompanyType } from '@/types/work_experience';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function ExperienceCard({ company }: { company: CompanyType }) {
-  const [openItems, setOpenItems] = useState<{ [key: string]: boolean }>({});
-  const [hasInteracted, setHasInteracted] = useState<{
-    [key: string]: boolean;
-  }>({});
-
   const experience = WORK_EXPEREINCES.find((exp) => exp.company === company);
   if (!experience) {
     return null;
   }
-  const toggleItem = (title: string) => {
-    setOpenItems((prevOpenItems) => ({
-      ...prevOpenItems,
-      [title]: !prevOpenItems[title],
-    }));
-    setHasInteracted((prevHasInteracted) => ({
-      ...prevHasInteracted,
-      [title]: true,
-    }));
-  };
-
-  const getClassName = (title: string) => {
-    if (openItems[title]) {
-      return styles.open;
-    }
-    if (hasInteracted[title]) {
-      return styles.hide;
-    }
-    return '';
-  };
 
   return (
     <div className={styles.card}>
@@ -53,19 +25,7 @@ export default function ExperienceCard({ company }: { company: CompanyType }) {
       </div>
       <ul className={styles.do_list}>
         {experience.projects.map((item) => (
-          <li
-            className={`${styles.do_item} ${getClassName(item.title)}`}
-            key={item.title}
-          >
-            <button
-              onClick={() => toggleItem(item.title)}
-              type="button"
-              className={styles.detail_toggle}
-            >
-              <span className="a11yHidden">
-                {item.title} 내용 자세히 더 보기
-              </span>
-            </button>
+          <li className={styles.do_item} key={item.title}>
             <h6 className={styles.title}>{item.title}</h6>
             <ul className={styles.description}>
               {item.details.map((detail) => (
