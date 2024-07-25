71fecf99e968b7fa0ab3c2a60bde19b6b02afa85
sryung <sryung1225@gmail.com>
2024-07-25 17:18:04 +0900

    [🎨 style] 반응형 레이아웃 점검 및 수정 #10 - 업무 경험 순서 변경, 헤더 z-index 값 수정 등

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -26,8 +26,8 @@ export default function Work() {
       }
     >
       <div className={styles.wrapper}>
-        <ExperienceCard company="티몬" />
         <ExperienceCard company="멋쟁이사자처럼" />
+        <ExperienceCard company="티몬" />
       </div>
     </Section>
   );
