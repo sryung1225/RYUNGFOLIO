50db3b920389c16bd3655fe58fcb238e4bb3b4cf
sryung <sryung1225@gmail.com>
2024-08-04 20:33:56 +0900

    [🎨 style] 메일 전송 알럿 스타일링 #18

--- src/components/contact/Alert.tsx
+++ src/components/contact/Alert.tsx
@@ -1,5 +1,15 @@
 import styles from '@/styles/components/contact/Alert.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Alert({ message }: { message: string }) {
-  return <p className={styles.alert}>{message}</p>;
+  const messages: string[] = message.split('\n');
+  return (
+    <div className={styles.alert}>
+      {messages.map((text) => (
+        <p className={styles.message} key={uuidv4()}>
+          {text}
+        </p>
+      ))}
+    </div>
+  );
 }
