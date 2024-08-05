6fd417f9e3c6c0afb143ca80ad6e356cf39f1f61
sryung <sryung1225@gmail.com>
2024-08-05 18:51:15 +0900

    [✨ feat] 프로젝트 개별 데이터 연동 #16

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -3,10 +3,19 @@ import instance from './axiosInstance';
 async function getProjects() {
   try {
     const response = await instance.get(`/projects`);
-    console.log('데이터 패칭 성공', response.data);
     return response.data;
   } catch (error) {
-    console.error('데이터 패칭 실패', error);
+    console.error('프로젝트 데이터 패칭 실패', error);
+    return [];
+  }
+}
+
+export async function getProjectDetail({ id }: { id: string }) {
+  try {
+    const response = await instance.get(`/projects?id=eq.${id}`);
+    return response.data[0];
+  } catch (error) {
+    console.error('개별 프로젝트 데이터 패칭 실패', error);
     return [];
   }
 }
