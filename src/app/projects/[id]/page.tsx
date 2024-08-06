d6a34caac43513560f5be895d4303a5f18cb1bcb
sryung <sryung1225@gmail.com>
2024-08-06 17:37:29 +0900

    [✨ feat] ProjectDetail 페이지 마크업 및 스타일링 #16
    - 1차 작업 완료
    - 잔여 작업: 링크(배포, 깃헙, 포스팅) / 프로젝트 관련 기술(주요 기능, 작업 내용, 회고)

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,31 +1,61 @@
+import Link from 'next/link';
 import Image from 'next/image';
+import styles from '@/styles/projects/ProjectDetail.module.scss';
 import { getProjectDetail } from '@/services/projects';
 import type { ProjectType } from '@/types/project';
+import formattedPeriod from '@/utills/formattedPeriod';
+import formattedSkillName from '@/utills/formattedSkillName';
 
 export default async function ProjectDetailPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const id = params;
-  const project: ProjectType = await getProjectDetail(id);
-  console.log('프로젝트 개별 데이터: ', project);
+  const project: ProjectType = await getProjectDetail(params);
   return (
-    <section>
-      <h2>{project.title}</h2>
-      <p>
-        {project.startDate} - {project.endDate}
-      </p>
-      <p>{project.skills}</p>
-      <p>{project.member}</p>
-      <p>{project.role}</p>
+    <section className={styles.wrapper}>
+      <Link href="/#projects" className={styles.back}>
+        전체 프로젝트 보기
+      </Link>
+      <h2 className={styles.title} data-text={project.title}>
+        {project.title}
+      </h2>
+      <p className={styles.summary}>{project.summary}</p>
       <Image
+        className={styles.thumbnail}
         src={project.thumbnail || '/img/dummy.jpg'}
         alt={`${project.title} 대표 이미지`}
-        width="800"
-        height="300"
+        width="1120"
+        height="800"
         priority
       />
+      <h3 className="a11yHidden">프로젝트 정보</h3>
+      <dl className={styles.info}>
+        <dt>기한</dt>
+        <dd>{formattedPeriod(project.startDate, project.endDate)}</dd>
+        <dt>사용 기술</dt>
+        <dd>
+          {project.skills && (
+            <ul className={styles.skills}>
+              {project.skills.map((skill) => (
+                <li key={skill}>
+                  <Image
+                    src={`/img/skills/${formattedSkillName(skill)}.svg`}
+                    alt={skill}
+                    width="24"
+                    height="24"
+                  />
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          )}
+        </dd>
+        <dt>규모 / 인원</dt>
+        <dd>{project.member}</dd>
+        <dt>역할</dt>
+        <dd>{project.role}</dd>
+      </dl>
     </section>
   );
 }
