2f4cb031ad812db5d4a47ae78b092c1782c1c45a
sryung <sryung1225@gmail.com>
2024-08-07 19:07:31 +0900

    [✨ feat] 스크롤 변화 로직 커스텀 훅으로 생성 #19
    - 스크롤이 특정 구간(id + offset)에 도달하는 여부를 판단하기 위한 로직을 다른 컴포넌트에서 사용할 수 있도록 커스텀 훅으로 분리

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,26 +1,11 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import useScrolledPast from '@/hooks/useScrolledPast';
 import styles from '@/styles/common/Header.module.scss';
 
 export default function Header() {
-  const [isScrolledPast, setIsScrolledPast] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      const aboutElement = document.getElementById('about');
-      if (aboutElement) {
-        const aboutPosition = aboutElement.getBoundingClientRect().top;
-        if (aboutPosition <= 0) setIsScrolledPast(true);
-        else setIsScrolledPast(false);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+  const isScrolledPast = useScrolledPast('about', 0);
   return (
     <header
       className={isScrolledPast ? styles.header_white : styles.header_black}
