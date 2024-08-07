72883280e4c9ba87d26b9bd9671a7e5a6d6e60b0
sryung <sryung1225@gmail.com>
2024-08-07 18:05:39 +0900

    [🎨 style] Header 컴포넌트 스타일링 #19

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -33,9 +33,7 @@ export default function Header() {
         height="40"
         priority
       />
-      {isScrolledPast && (
-        <span className={styles.email}>sryung1225@gmail.com</span>
-      )}
+      <span className={styles.name}>RYUNGFOLIO</span>
     </header>
   );
 }
