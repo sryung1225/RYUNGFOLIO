006dc8e47dd5bdbdf236d325e7145d0959120f9c
sryung <sryung1225@gmail.com>
2024-08-09 21:21:41 +0900

    [⏪ revert] useScrolledPast 커스텀훅 제거 #16
    - 244b217과 연관. 해당 커스텀훅을 사용하는 컴포넌트가 Header 하나만 남게 되었기에 분리할 필요성이 없어짐. 커스텀 훅 제거 결정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,26 @@
 'use client';
 
 import Image from 'next/image';
-import useScrolledPast from '@/hooks/useScrolledPast';
+import { useEffect, useState } from 'react';
 import styles from '@/styles/common/Header.module.scss';
 
 export default function Header() {
-  const isScrolledPast = useScrolledPast('about', 0);
+  const [isScrolledPast, setIsScrolledPast] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = document.getElementById('about');
+      if (element) {
+        const position = element.getBoundingClientRect().top;
+        if (position <= 0) setIsScrolledPast(true);
+        else setIsScrolledPast(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <header
       className={isScrolledPast ? styles.header_white : styles.header_black}
